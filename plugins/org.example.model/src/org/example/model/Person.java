939d9494b42a60dcbc2ef02f30cb86114d21409d
Mata Shashidhara (uib03395) <shashidhara.2.mata@continental-corporation.com>
2026-05-16 13:52:01 +0530

    Created miniDsl plug-ins and sharing under miniDsl repo

---
+++ plugins/org.example.model/src/org/example/model/Person.java
@@ -0,0 +1,19 @@
+package org.example.model;
+
+public class Person {
+
+	private String name;
+	private String gender; 
+	private int age; 
+	
+	public Person(String name, String gender, int age) { 
+		this.name = name; 
+		this.gender = gender; 
+		this.age = age; 
+	}
+	
+	public String getName() { return name; } 
+	public String getGender() { return gender; } 
+	public int getAge() { return age; }
+
+}
